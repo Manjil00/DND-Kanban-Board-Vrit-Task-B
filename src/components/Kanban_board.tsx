e2c6c1d2b2e8428f2676c668bdc7ad88feb89366
ACER <210220@softwarica.edu.np>
2024-11-28 00:08:43 +0545

    draggable and swappable dnd added

--- src/components/Kanban_board.tsx
+++ src/components/Kanban_board.tsx
@@ -1,12 +1,19 @@
-import { useState } from 'react';
+import { DndContext, DragEndEvent, DragOverlay, DragStartEvent } from "@dnd-kit/core";
+import { arrayMove, SortableContext } from "@dnd-kit/sortable";
+import { useMemo, useState } from 'react';
+import { createPortal } from 'react-dom';
 import { ID, Section } from '../types';
 import Plus from './icons/plus';
 import SectionContainer from './SectionContainer';
 
-const Kanban_board = () => {
+function Kanban_board(){
 
     const [section, setSection]= useState<Section[]>([]);
-    console.log(section);
+    const sectionId= useMemo(() => section.map((sec)=>sec.id),[section]);
+
+    //activesection
+    const [activeColumn,setActiveColumn]= useState<Section | null>(null);
+
 
 function addSection(){
 const AddSection: Section={
@@ -26,14 +33,52 @@ function deleteSection(id: ID){
     setSection(filterSection);
 }
 
+//DraggStart
+function OnDragStart(event: DragStartEvent){
+    console.log("dragstart",event);
+    if(event.active.data.current?.type ==="Section"){
+        setActiveColumn(event.active.data.current.section);
+        return;
+    }
+}
+
+//end
+function OnDragEnd(event:DragEndEvent){
+    const {active,over}=event;
+    if(!over)
+        return;
+
+    const activeSectionId=active.id;
+    const overSectionId=over.id;
+
+    if (activeSectionId===overSectionId)
+        return;
+
+    setSection((section)=>{
+        const activeSectionIndex =section.findIndex(
+            (sec) => sec.id===activeSectionId
+        );
+
+        const overSectionIndex =section.findIndex(
+        (sec) => sec.id===overSectionId
+        );
+
+        return arrayMove(section,activeSectionIndex,overSectionIndex);
+    })
+}
+
 return (
-    <div className=' w-auto h-[100vh] p-5'>
+    <DndContext onDragStart={OnDragStart} onDragEnd={OnDragEnd}>
+            <div className=' w-auto h-[100vh] p-5'>
     <h1 className='text-4xl font-serif text-center'>Kanban Board</h1>
 
 <div className="Section flex justify-evenly items-center gap-3">
 
 <div className='flex justify-evenly items-center gap-4 p-3'>
+    <SortableContext items={sectionId}>
     {section.map(sec => <div><SectionContainer key={sec.id} section={sec} deleteSection={deleteSection}/></div>)}
+    </SortableContext>
+    
 </div>
 
 
@@ -46,6 +91,13 @@ return (
 </div>
 
     </div>
+
+{createPortal(<DragOverlay>
+        {activeColumn && <SectionContainer section={activeColumn} deleteSection={deleteSection}/>}
+    </DragOverlay>,document.body
+)};
+
+</DndContext>
 )
 }
 
--- src/components/SectionContainer.tsx
+++ src/components/SectionContainer.tsx
@@ -1,6 +1,7 @@
+import { useSortable } from "@dnd-kit/sortable";
 import { Section, ID } from "../types";
 import DeleteIcon from "./icons/DeleteIcon";
-
+import {CSS} from "@dnd-kit/utilities";
 interface Props{
     section:Section;
     deleteSection:(id:ID) => void;
@@ -9,11 +10,32 @@ interface Props{
 function SectionContainer(props:Props) {
 
 const {section, deleteSection} =props;
+
+    const {setNodeRef, attributes, listeners, transform, transition,isDragging}
+    =useSortable({
+        id:section.id,
+        data:{
+            type:"Section",
+            section,
+        },
+    });
+
+    const style={
+        transition,
+        transform:CSS.Transform.toString(transform),
+    };
+
+    if(isDragging){
+        return<div ref={setNodeRef} style={style} className="bg-slate-500 h-[500px] w-[400px] text-black rounded-xl"></div>
+    }
+
     return (
-    <div className="bg-black h-[500px] w-[400px] text-black rounded-xl">
+    <div ref={setNodeRef}
+    style={style}
+    className="bg-black h-[500px] w-[400px] text-black rounded-xl">
         <div className="top p-5 bg-slate-500 h-[50px] w-full mx-auto rounded-xl rounded-b-none flex justify-between  items-center">
             <div className="number bg-black rounded-full h-[20px] w-[20px] text-white text-center ">0</div>
-            <h1 className=" ">{section.title}</h1>
+            <h1 {...attributes} {...listeners}>{section.title}</h1>
             <button><DeleteIcon/></button>
         </div>
 
